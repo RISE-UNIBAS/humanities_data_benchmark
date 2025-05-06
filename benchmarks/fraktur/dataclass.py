6de2bd72d2bec647e0b611ff8e453a8068057c7e
MHindermann <maximilian.hindermann@gmail.com>
2025-05-06 14:05:36 +0200

    start implementing fraktur

---
+++ benchmarks/fraktur/dataclass.py
@@ -0,0 +1,16 @@
+from typing import Dict, Optional, List
+from dataclasses import dataclass, field
+from pydantic import BaseModel
+
+
+@dataclass
+class Advertisement:
+    date: Optional[str] = None  # date in ISO 8601 format
+    tags_section: Optional[str] = None  # title of the section in which an advertisement is placed
+    ntokens: Optional[int] = None  # number of tokens of an advertisement
+    text: Optional[str] = None  # text of the advertisement
+
+
+@dataclass
+class Document(BaseModel):
+    advertisements: Dict[str, List[Advertisement]] = field(default_factory=dict)
