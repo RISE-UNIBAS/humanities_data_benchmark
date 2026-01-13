f9bbfb773723921fa8af165483176e64789b51ad
Maximilian Hindermann <maximilian.hindermann@gmail.com>
2026-01-13 14:31:33 +0100

    add docstrings

--- benchmarks/fraktur_adverts/dataclass.py
+++ benchmarks/fraktur_adverts/dataclass.py
@@ -3,11 +3,22 @@ from pydantic import BaseModel
 
 
 class Advertisement(BaseModel):
-    date: Optional[str] = None  # date in ISO 8601 format
-    tags_section: Optional[str] = None  # title of the section in which an advertisement is placed
-    ntokens: Optional[int] = None  # number of tokens of an advertisement
-    text: Optional[str] = None  # text of the advertisement
+    """Represents a single advertisement from a Fraktur document.
+
+    Attributes:
+        date: Date in ISO 8601 format
+        tags_section: Title of the section in which an advertisement is placed
+        text: Text of the advertisement
+    """
+    date: Optional[str] = None
+    tags_section: Optional[str] = None
+    text: Optional[str] = None
 
 
 class Document(BaseModel):
+    """Represents a document containing multiple advertisements.
+
+    Attributes:
+        advertisements: List of advertisements in the document
+    """
     advertisements: List[Advertisement] = []
--- benchmarks/library_cards/dataclass.py
+++ benchmarks/library_cards/dataclass.py
@@ -4,15 +4,37 @@ from pydantic import BaseModel
 
 
 class WorkType(BaseModel):
+    """Represents the type of work referenced in a library card.
+
+    Attributes:
+        type: Type of work, either "Dissertation or thesis" or "Reference"
+    """
     type: Literal["Dissertation or thesis", "Reference"]
 
 
 class Author(BaseModel):
+    """Represents an author on a library card.
+
+    Attributes:
+        last_name: Author's last name
+        first_name: Author's first name
+    """
     last_name: str
     first_name: str
 
 
 class Publication(BaseModel):
+    """Represents publication information from a library card.
+
+    Attributes:
+        title: Title of the publication
+        year: Year of publication
+        place: Place of publication
+        pages: Number of pages or page range
+        publisher: Publisher name
+        format: Format of the publication
+        editor: Editor name if applicable
+    """
     title: str
     year: str
     place: Optional[str] = None
@@ -23,11 +45,25 @@ class Publication(BaseModel):
 
 
 class LibraryReference(BaseModel):
+    """Represents library-specific reference information.
+
+    Attributes:
+        shelfmark: Library shelfmark or call number
+        subjects: Subject classifications or tags
+    """
     shelfmark: Optional[str] = None
     subjects: Optional[str] = None
 
 
 class Document(BaseModel):
+    """Represents a complete library card document.
+
+    Attributes:
+        type: Type of work
+        author: Author information
+        publication: Publication details
+        library_reference: Library reference information
+    """
     type: WorkType
     author: Author
     publication: Publication
